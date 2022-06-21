a0a2ccb1bdae8573129d04606b3ae48f58d0bd3a
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-06-20 20:22:31 -0400

    Added abbreviations for git commands

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,5 +2,19 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Set Ruby path for Arch
 export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
 export PATH="$PATH:$GEM_HOME/bin"
+
+# Abbreviations
+abbr gcm "git commit -m"
+abbr gs "git status"
+abbr ga "git add"
+abbr gl "git log"
+abbr gch "git checkout"
+abbr gchb "git checkout -b"
+abbr gb "git branch"
+abbr gr "git reset"
+abbr grh "git reset --hard"
+abbr gf "git fetch"
+abbr gp "git pull"
